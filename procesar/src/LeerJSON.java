2d68e3815770fdaad10ef5021acaa4c82b201d35
mgarclor <mgarclor@everis.nttdata.com>
2021-05-12 09:24:09 +0200

    Añadidos comentarios

--- procesar/src/LeerJSON.java
+++ procesar/src/LeerJSON.java
@@ -4,6 +4,12 @@ import org.json.JSONTokener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+/** @file LeerJSON.java
+ *  @class LeerJSON
+ *  @brief Representa la clase que se va a encargar de obtener los jobs de uno de los ficheros cuya ruta está en config.json,
+ *  que tiene un constructor al cual se le pasa como argumento un String, que representa la ruta de entrada para ese fichero
+ */
+
 public class LeerJSON {
     private FileInputStream in;
     LeerJSON(String rutaEntrada) {
@@ -13,6 +19,13 @@ public class LeerJSON {
             e.printStackTrace();
         }
     }
+
+    /**
+     * @brief La función obtenerElemento se encarga de obtener de uno de los archivos contemplados en config.json, obtenido
+     * a partir de su ruta de entrada, un array con los jobs que hay en ese archivo.
+     *
+     * @return El array de elementos JSON con todos los objetos que contiene el archivo para poder procesarlo.
+     */
 
     public JSONArray obtenerElemento(){
         JSONTokener tokener = new JSONTokener(this.in);
